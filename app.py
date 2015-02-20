9bc07db3b5d553155bab3004e5212393d27998a1
mfalce <falce.matthieu@gmail.com>
2015-02-20 23:28:29 +0100

    the prototype seems to work, but huge performance issues

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import time
 from threading import Thread
+import time
 
 from gevent import monkey
 monkey.patch_all()
@@ -7,19 +7,56 @@ monkey.patch_all()
 from flask import Flask, render_template
 from flask.ext.socketio import SocketIO, emit
 
+import zmq.green as zmq
+
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+port = "5556"
+thread = None
+
+# Socket to talk to server
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+socket.connect("tcp://localhost:{}".format(port))
+topicfilter = "".encode()
+socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
+
+def messages_gateway():
+	while True:
+		m = socket.recv_string()
+		m = m.decode()
+		zip_code, temp = m.split(" ")
+		print(temp)
+		socketio.emit("graph", {"data": int(temp)}, namespace='/test')
+		if m == 'quit':
+			print 'exiting.'
+			break
+
+def background_thread():
+	"""Example of how to send server generated events to clients."""
+	count = 0
+	while True:
+		time.sleep(1)
+		count += 1
+		socketio.emit('graph',
+		{'data': 'Server generated event', 'count': count},
+		namespace='/test')
+
 @app.route("/")
 def index():
+	global thread
+	if not thread:
+		thread = Thread(target=messages_gateway)
+		thread.start()
 	return render_template('index.html')
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
-	print('olo')
 	emit('my response', {'data': 'Connected', 'count': 0})
+	emit("graph", {"data":"5"})
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0")
