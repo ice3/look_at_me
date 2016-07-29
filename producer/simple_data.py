57e7e3d02c6249b2db52d5d3b86909ccca278386
Matthieu Falce <falce.matthieu@gmail.com>
2016-07-30 00:00:35 +0200

    python sends multiple data

--- producer/simple_data.py
+++ producer/simple_data.py
@@ -8,6 +8,8 @@ fps = 10.0
 i = 0.0
 while 1:
     x= int(math.cos(i/100)*1000)/100.0
+    y= int(math.sin(i/100)*1000)/100.0
     look_at_me.send("cos", [x])
+    look_at_me.send("sin", [y])
     time.sleep(1.0/fps)
     i+=1
